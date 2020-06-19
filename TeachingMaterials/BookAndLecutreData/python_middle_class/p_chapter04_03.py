a82651573dd6654a15b92e30dfb12711209c10f2
Jaehyeong Kim <imn00133@gmail.com>
2020-06-19 12:58:31 +0900

    made: studying chapter04_03 in python middle class

---
+++ TeachingMaterials/BookAndLecutreData/python_middle_class/p_chapter04_03.py
@@ -0,0 +1,34 @@
+print(__builtins__.__dict__)
+
+t1 = (10, 20, (30, 40, 50))
+t2 = (10, 20, [30, 40, 50])
+
+print(hash(t1))
+# print(hash(t2))
+
+print()
+
+source = (('k1', 'val1'),
+          ('k1', 'val2'),
+          ('k2', 'val3'),
+          ('k2', 'val4'),
+          ('k2', 'val5'))
+
+new_dict1 = {}
+new_dict2 = {}
+
+for k, v in source:
+    if k in new_dict1:
+        new_dict1[k].append(v)
+    else:
+        new_dict1[k] = [v]
+
+print(new_dict1)
+
+for k, v in source:
+    new_dict2.setdefault(k, []).append(v)
+
+print(new_dict2)
+
+new_dict3 = {k: v for k, v in source}
+print(new_dict3)
